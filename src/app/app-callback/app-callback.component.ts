aedbca0c271ac998d9011bde829009d7a26a6322
ketsumi <mewtropolis.ketsumi@gmail.com>
2019-05-10 13:44:15 -0400

    added dependencies

---
+++ src/app/app-callback/app-callback.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-callback',
+  templateUrl: './app-callback.component.html',
+  styleUrls: ['./app-callback.component.sass']
+})
+export class AppCallbackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppLoginComponent } from './app-login/app-login.component';
+import { AppCallbackComponent } from './app-callback/app-callback.component';
+import { AppLogoutComponent } from './app-logout/app-logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppLoginComponent,
+    AppCallbackComponent,
+    AppLogoutComponent
   ],
   imports: [
     BrowserModule,
